4691863522d7b5b139df81158f437dab4117e692
Home <Home@MacBook-Air.local>
2025-05-04 17:55:24 +0400

    “განახლება”

---
+++ level 61/classwork/cw.js
@@ -0,0 +1,45 @@
+// cw1
+// internal js-ის გამოყენებით კონსოლში ჯერ დაბეჭდეთ თქვენი სახელი, შემდეგ გვარი და ბოლოს რომელ აკადემიაში სწავლობთ
+console.log("tornike");
+console.log("khozrevanidze");
+console.log("goa");
+
+
+
+// cw2
+// შექმენით 5 ცვლადი და თითოეულში შეინახეთ განსხვავებული ინფორმაცია. console-ში გამოიტანეთ თითოეული ცვლადის მნიშვნელობა
+let first = 1
+let second = 2
+let third = 3
+let fourth = 4
+let fifth = 5
+
+console.log(first)
+console.log(second)
+console.log(third)
+console.log(fourth)
+console.log(fifth)
+
+
+
+
+// cw3
+// შექმენით 3 ცვლადი და დაბეჭდეთ თითოეულის მნიშვნელობა.
+// შემდეგ სამივე ცვლადს შეუცვალეთ მნიშვნელობები და დაბეჭდეთ უკვე განახლებული მნიშვნელობები.
+// ყოველთვის დაბეჭდეთ console-ში.
+
+let i = "i"
+let b = 'b'
+let c = 'c'
+
+console.log(i)
+console.log(b)
+console.log(c)
+
+i = 'd'
+b = 'e'
+c = 'f'
+
+console.log(i)
+console.log(b)
+console.log(c)
